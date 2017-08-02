4d071640e3d3fcf0756a4524254d4ef7d0791f13
Matthew Day <matt@matthewday.net>
2017-08-02 15:00:47 -0400

    Add HTML, CSS, and JS from copied straight from JSFiddle

--- comments/comments.md
+++ comments/comments.md
@@ -2,3 +2,6 @@
 
 ##### c001
 Source: https://gist.github.com/octocat/9257657
+
+##### c002
+We can turn this on to verify that the DOM can properly access JS functions.
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,18 @@
       alert('JS is hooked up correctly.');
     },
     init: function() {
-      app.testAlert();
+      //app.testAlert();  //c002
+      app.mobileToggle();
+    },
+    mobileToggle: function() {
+      $(document).on('click', '#mobile', function() {
+        $('html').toggleClass('disable');
+        $('#mobile').toggleClass('active');
+        $('#themobilemenu').toggleClass('active');
+        $('#thenav').toggleClass('inactive');
+        $('#thenimg').toggleClass('opacity');
+        $('#theoverlay').toggleClass('active');
+      });
     }
   };
 
