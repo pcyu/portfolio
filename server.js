975788c20da87b1b187b76d185cf80be8d74a2aa
Peter Yu <PeterYu@gmail.com>
2018-04-13 15:55:17 -0400

    removed some boilerplate code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use( '/', express.static(__dirname + '/public') );
 app.use( '/node_modules', express.static(__dirname + '/node_modules') );
