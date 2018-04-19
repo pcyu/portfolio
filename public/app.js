3e5f29b6b2133e28ef6d6d8fe21a2ed81a865710
Peter Yu <PeterYu@gmail.com>
2018-04-19 14:06:52 -0400

    removed responsive box used for dev purposes

--- public/app.js
+++ public/app.js
@@ -1,21 +1,8 @@
 (function($) {
 
   var app = {
-    box: function(){
-      $('head').append('<style>.viewport{background:rgba(0,0,0,0.7);bottom:0;color:white;cursor:pointer;font-size:10px;font-family:"LucidaGrande","Helvetica",sans-serif;line-height:14px;padding-bottom:4px;padding-left:6px;padding-right:6px;padding-top:4px;position:fixed;right:0;z-index:999}.viewport.inactive{opacity:0}#responsive-width,#responsive-height{display:inline-block}</style>');
-      $('body').append('<div class="viewport"><div id="responsive-width"></div> x <div id="responsive-height"></div></div>');
-    },
-    calculateDimensions: function(){
-      var viewportWidth = window.innerWidth;
-      var viewportHeight = window.innerHeight;
-      $("#responsive-width").html(viewportWidth);
-      $("#responsive-height").html(viewportHeight);
-    },
     init: function() {
-      //app.testAlert();  //c002
-      app.box();
       app.mobileToggle();
-      app.sizer();  //c007
     },
     mobileToggle: function() {  //c009
       $(document).on('click', '#t-mobileicon', function() {
@@ -24,13 +11,6 @@
         $('#t-navitems').toggleClass('active');
         $('#t-overlay').toggleClass('active');
       });
-    },
-    sizer: function() {
-      app.calculateDimensions();
-      app.toggleBox();
-    },
-    testAlert: function() {
-      alert('JS is hooked up correctly.');
     },
     toggleBox: function(){
       $('.viewport').click(function(){
