7d828927f2e05280e31cb9ed3c5c5e51099b842f
Peter Yu <PeterYu@gmail.com>
2018-04-19 14:34:58 -0400

    added smooth scroll

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@
   var app = {
     init: function() {
       app.mobileToggle();
+      app.scroll();
     },
     mobileToggle: function() {  //c009
       $(document).on('click', '#t-mobileicon', function() {
@@ -12,19 +13,21 @@
         $('#t-overlay').toggleClass('active');
       });
     },
-    toggleBox: function(){
-      $('.viewport').click(function(){
-        $(this).toggleClass('inactive');
+    scroll: function(){
+      $('.scroll').on("click", function(e){
+        if (this.hash !== "") {
+          e.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 600, function(){
+            window.location.hash = hash;
+          });
+        }
       });
     }
   };
-
   $(window).on('load', () => {
     app.init();
   });
-
-  $(window).on('resize', () => {
-    app.sizer();  //c007
-  });
-
 })(window.jQuery);
