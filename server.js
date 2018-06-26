bafa635173ebcbc6778108c1929b1ae177cea0f4
Peter <peteryu@gmail.com>
2018-06-26 17:52:09 -0400

    took out heartbeat from  matt's code

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ app.use( '/', express.static(__dirname + '/public') );
 app.use( '/node_modules', express.static(__dirname + '/node_modules') );
 app.use( '/src', express.static(__dirname + '/src') );
 
-app.get('/heartbeat', function(req, res) {
-  res.json({
-    is: 'working'
-  })
-});
-
 app.listen(PORT, function() {
   console.log(`The server at port ${PORT} is listening.`);
 });
